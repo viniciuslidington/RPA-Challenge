72f270d50be4d3aa77b2371b8c007881d577420d
viniciuslidington <vlidington@gmail.com>
2024-05-16 23:48:08 -0300

    Automatizacao geral

--- RPA.py
+++ RPA.py
@@ -1,3 +1,30 @@
+#Ambiente virtual
+import subprocess
+import sys
+import os
+
+def create_virtual_env(venv_dir):
+    subprocess.check_call([sys.executable, "-m", "venv", venv_dir])
+    print(f"Ambiente virtual criado em {venv_dir}")
+
+def install_package(package, venv_dir):
+    pip_executable = os.path.join(venv_dir, 'bin', 'pip') if os.name != 'nt' else os.path.join(venv_dir, 'Scripts', 'pip')
+    subprocess.check_call([pip_executable, "install", package])
+
+# Diretório do ambiente virtual
+venv_dir = 'my_env'
+
+# Criar ambiente virtual
+create_virtual_env(venv_dir)
+
+# Lista de pacotes a serem instalados
+packages = ['selenium', 'pandas']
+
+# Instalar pacotes dentro do ambiente virtual
+for package in packages:
+    install_package(package, venv_dir)
+
+#importar bibliotecas
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
@@ -27,16 +54,12 @@ navegador.find_element("xpath","/html/body/app-root/div[2]/app-rpa1/div/div[1]/d
 file_name = "challenge.xlsx"
 file_path = os.path.join(download_path, file_name)
 #baixar o arquivo e salvar diretamento no PATH para abrir automaticamente
-
-
 time.sleep(2)
 tabela = pd.read_excel("challenge.xlsx")
-
 navegador.find_element("xpath","/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button").click()
 
-#loop para capturar as informações da tabela
-
-for index, row in tabela.iterrows():
+#funcao para capturar as informações da tabela
+def preencher_formulario(navegador, row):
     first_name = row['First Name']
     last_name = row['Last Name ']
     company_name = row['Company Name']
@@ -44,23 +67,24 @@ for index, row in tabela.iterrows():
     address = row['Address']
     email = row['Email']
     phone_number = row['Phone Number']
-
-    #Company Name
+    
+    # Preencher os campos do formulário
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelCompanyName"]').send_keys(company_name)
-    #Phone Number
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelPhone"]').send_keys(str(phone_number))
-    #Email
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelEmail"]').send_keys(email)
-    #Role in Company
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelRole"]').send_keys(role)
-    #Last Name
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelLastName"]').send_keys(last_name)
-    #Address
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelAddress"]').send_keys(address)
-    #First Name
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelFirstName"]').send_keys(first_name)
-    #Enviar formulário
+    
+    # Enviar o formulário
     navegador.find_element(By.XPATH, '/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input').click()
+
+def processar_tabela(tabela, navegador):
+    for index, row in tabela.iterrows():
+        preencher_formulario(navegador, row)
+
+processar_tabela(tabela,navegador)
 
 time.sleep(5)
 navegador.quit()
