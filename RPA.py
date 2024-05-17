8c6834059486ea4de785616d22e6ab94adde40da
viniciuslidington <vlidington@gmail.com>
2024-05-16 23:16:44 -0300

    RPA.py

--- RPA.py
+++ RPA.py
@@ -1,30 +1,51 @@
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+import os
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-navegador = webdriver.Chrome()
+#acessar o diretorio
+download_path = os.getcwd()
+
+#config web driver
+chrome_options = Options()
+chrome_options.add_experimental_option("prefs", {
+    "download.default_directory": download_path,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+})
+service = Service()
+navegador = webdriver.Chrome(service=service, options=chrome_options)
+
 navegador.get("https://www.rpachallenge.com/")
-time.sleep(1)
+time.sleep(2)
 navegador.find_element("xpath","/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/a").click()
 
+file_name = "challenge.xlsx"
+file_path = os.path.join(download_path, file_name)
 #baixar o arquivo e salvar diretamento no PATH para abrir automaticamente
-time.sleep(1)
-navegador.find_element("xpath","/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button").click()
 
+
+time.sleep(2)
 tabela = pd.read_excel("challenge.xlsx")
 
+navegador.find_element("xpath","/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button").click()
 
-#criar def para função
 #loop para capturar as informações da tabela
 
-for i, first_name in enumerate(tabela['First Name']):
-    last_name = tabela.loc[i, 'Last Name ']
-    company_name = tabela.loc[i, 'Company Name']
-    role = tabela.loc[i, 'Role in Company']
-    address = tabela.loc[i, 'Address']
-    email = tabela.loc[i, 'Email']
-    phone_number = tabela.loc[i, 'Phone Number']
+for index, row in tabela.iterrows():
+    first_name = row['First Name']
+    last_name = row['Last Name ']
+    company_name = row['Company Name']
+    role = row['Role in Company']
+    address = row['Address']
+    email = row['Email']
+    phone_number = row['Phone Number']
 
     #Company Name
     navegador.find_element(By.CSS_SELECTOR, '[ng-reflect-name="labelCompanyName"]').send_keys(company_name)
